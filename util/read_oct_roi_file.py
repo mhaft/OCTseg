e7a2764e8106d5377216b5daa84bb793e58149ec
Mohammad Haft-Javaherian <mh973@cornell.edu>
2019-07-21 15:48:16 -0400

    fix duplicate datapoints

--- util/read_oct_roi_file.py
+++ util/read_oct_roi_file.py
@@ -36,11 +36,11 @@ def roi_file_parser(file_path):
 def lumen_iel_mask(obj_list, im_shape):
     """generate lumen or IEL mask based on the point list."""
     obj_list = np.array(obj_list) - 1 # match 1-index to 0-index
-    x, idx = np.unique(obj_list[:, 1], return_index=True)
-    obj_list = obj_list[idx]
+    x, idx, c = np.unique(obj_list[:, 1], return_index=True, return_counts=True)
+    obj_list = obj_list[idx[c < 2]]
     obj_list = np.concatenate((obj_list - [0, im_shape[-1], 0], obj_list, obj_list +
                                [0, im_shape[-1], 0]), axis=0)
-    f = interp1d(obj_list[:, 1], obj_list[:, 0], kind='cubic')
+    f = interp1d(obj_list[:, 1], obj_list[:, 0], kind='quadratic')
     y_lim = np.tile(f(np.arange(im_shape[-1])).T, reps=(im_shape[-2], 1))
     y = np.tile(np.arange(im_shape[-2]).T, reps=(im_shape[-1], 1)).T
     return y <= y_lim
@@ -60,10 +60,10 @@ def read_oct_roi_file(file_path, im_shape):
     for gw in obj_list['GW']:
         z = gw[1][2] - 1
         gw = np.array([gw[1][1], gw[2][1]])
-        if gw.ptp() < (im_shape[-1]/2):
-            out[z, :, (gw[0] - 1):gw[1]] = 1
+        if gw[0] <= gw[1]:
+            out[z, :, gw[0]:(gw[1] + 1)] = 1
         else:
-            out[z, :, :gw[0]] = 1
-            out[z, :, (gw[1] - 1):] = 1
+            out[z, :, gw[0]:] = 1
+            out[z, :, :(gw[1] + 1)] = 1
     return out
 
