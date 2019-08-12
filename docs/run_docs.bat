e61212bf0bac0e0e750f8c925e03b4833e180b2d
mhaft <mh973@cornell.edu>
2019-08-12 18:25:21 -0400

    add pause and anaconda cmd script

--- docs/run_docs.bat
+++ docs/run_docs.bat
@@ -1,2 +1,5 @@
-call make.bat html
+call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
 call make.bat latex
+call build\latex\make.bat
+call make.bat html
+PAUSE
--- tests/run-test.bat
+++ tests/run-test.bat
@@ -1,1 +1,3 @@
+call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
 pytest -v --cov --cov-report html
+PAUSE
