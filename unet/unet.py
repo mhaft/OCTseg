fcba79a8f3e566979f01075bbdb232d9d3d33ef8
mhaft <mh973@cornell.edu>
2021-03-29 17:34:35 -0400

    Add dropout for pooling larger than 2x

--- unet/unet.py
+++ unet/unet.py
@@ -28,8 +28,8 @@ def unet_model(im_shape, nFeature=32, outCh=2, nLayer=3, pool_scale=2):
     x = KL.Input(shape=im_shape)
     out = [x]
     for iLayer in range(nLayer):
-        h = conv_layer(out[-1], (2 ** (iLayer // 2)) * nFeature)
-        out.append(MaxPoolingND(h, s=pool_scale))
+        out.append(conv_layer(out[-1], (2 ** (iLayer // 2)) * nFeature))
+        out.append(MaxPoolingND(out[-1], s=pool_scale))
 
     out.append(conv_layer(out[-1], (2 ** (nLayer // 2)) * nFeature))
     out.append(conv_layer(out[-1], (2 ** (nLayer // 2)) * nFeature))
@@ -41,8 +41,10 @@ def unet_model(im_shape, nFeature=32, outCh=2, nLayer=3, pool_scale=2):
                                         (0, (im_shape[-2] + pool_scale - 1) // pool_scale * pool_scale - im_shape[-2])),
                               data_format=None)(u)
 
-        c = KL.Concatenate()([out[iLayer], u])
+        c = KL.Concatenate()([out[1 + 2 * iLayer], u])
         out.append(conv_layer(c, (2 ** (iLayer // 2)) * nFeature))
+        if iLayer != 0 and pool_scale > 2:
+            out.append(KL.Dropout(0.5)(out[-1]))
 
     out.append(conv_layer(out[-1], outCh))
 
